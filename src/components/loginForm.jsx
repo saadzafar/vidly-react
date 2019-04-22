57f3b2a4fcdf8d97cf5e1ed90c5ebb5790f0cfe5
m47ad0r <saad.zafar.7223@gmail.com>
2019-04-22 15:13:46 +0800

    Learning to use ref to access DOM (Testing)

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,14 +1,36 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  username = React.createRef();
+
+  // Note: replaced with autoFocus inside input field
+  // componentDidMount() {
+  //   this.username.current.focus();
+  // }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    // Call the server, make changes and redirect to different page
+    // Note: make minimal use of refs. use only when really need to access DOM directly
+    const username = this.username.current.value;
+    console.log("Submitted", username);
+  };
+
   render() {
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              autoFocus
+              ref={this.username}
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
