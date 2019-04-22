a533de239517816ef52d669908629ecc7c672a58
m47ad0r <saad.zafar.7223@gmail.com>
2019-04-22 15:36:27 +0800

    Bind state of input fields with state of component (loginform.jsx)

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,23 +1,26 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-  username = React.createRef();
-
-  // Note: replaced with autoFocus inside input field
-  // componentDidMount() {
-  //   this.username.current.focus();
-  // }
+  state = {
+    account: { username: "", password: "" }
+  };
 
   handleSubmit = e => {
     e.preventDefault();
 
     // Call the server, make changes and redirect to different page
-    // Note: make minimal use of refs. use only when really need to access DOM directly
-    const username = this.username.current.value;
-    console.log("Submitted", username);
+    console.log("Submitted");
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
   };
 
   render() {
+    const { account } = this.state;
+
     return (
       <div>
         <h1>Login</h1>
@@ -25,16 +28,25 @@ class LoginForm extends Component {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
+              value={account.username}
+              onChange={this.handleChange}
               autoFocus
-              ref={this.username}
               id="username"
+              name="username"
               type="text"
               className="form-control"
             />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              id="password"
+              name="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
